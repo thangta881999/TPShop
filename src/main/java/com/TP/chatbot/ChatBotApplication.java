49ed292d8cfc00efbe3a4aa23e60fbb41df97faf
thangta881999 <thangta881999@gmail.com>
2021-07-21 21:37:33 +0700

    thang2177

--- src/main/java/com/TP/chatbot/ChatBotApplication.java
+++ src/main/java/com/TP/chatbot/ChatBotApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatBotApplication {
     @Bean
-    public Messenger messenger(@Value("EABYiXO6jfBABAHE9am0Blv2mhuRWvHlDc6OEZAnBuf8PkognMTIwnmcKxmuq6uF3mqVav1H0ewD6ClGLbwGam4ga9A5VRUZAQY04beIBCi6ZBSvtdUQLTeE3CZAAEe4imznc2M9zAVZC36vLRAmTteeN4ouf0NczwQ0uQ9uHnZB4m4iarHxBMdmy21SihDHJkZD")  String PAGE_ACCESS_TOKEN,
+    public Messenger messenger(@Value("EABYiXO6jfBABAPDW2ZBoe7ZBnG2J7R8hqVRySsMShRzedkN7In5dXMobbhM6rKdZCIVZCZBZCQtocuYqrVA0J5MAqR3dGZBzPIAfkbpydSCUc4snEJfTbHkKJQfAmLRZAP9CTRkrZBZBseXxmoQ0ynTYlgjDi5KzPmHMjmNBbvFK5Ax8TjTavz0NYZBj4icLBo9uXMZD")  String PAGE_ACCESS_TOKEN,
                                @Value("bee261486564523cf86207e0fa06502a")  final String APPSECRET,
                                @Value("$123456789") final String VERIFY_TOKEN) {
         return Messenger.create(PAGE_ACCESS_TOKEN, APPSECRET, VERIFY_TOKEN);
